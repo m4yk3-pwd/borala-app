50bb22566e14d3fad80d52e19afaba83a9b38bbc
Mayke <mayke000000000@gmail.com>
2026-07-15 20:00:54 -0300

    chore: configurando FireBase

---
+++ borala/src/config/FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeHIfuKfrQPdL60vst9zII1heBYymoObA",
+  authDomain: "bora-la-d7c42.firebaseapp.com",
+  projectId: "bora-la-d7c42",
+  storageBucket: "bora-la-d7c42.firebasestorage.app",
+  messagingSenderId: "843483414454",
+  appId: "1:843483414454:web:2c193237f9cfa5ce258b17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
